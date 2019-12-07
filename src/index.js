826756272304af03d75f978e2053e98fd9fa2b2f
vadyaxio <vadya10dmb@gmail.com>
2019-12-07 20:23:32 +0700

    start dev

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Phaser from "phaser";
 import logoImg from "./assets/logo.png";
+import plantImg from "./assets/plant.jpg";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App.jsx";
@@ -13,19 +14,27 @@ const config = {
     create: create
   }
 };
+
 const game = new Phaser.Game(config);
+
 function preload() {
-  this.load.image("logo", logoImg);
+  this.load.image('plant', plantImg);
+  this.load.image('logo', logoImg);
 }
+
 function create() {
+  const plant = this.add.image(0, 0, 'plant').setOrigin(0, 0).setScale(2);
+  
   const logo = this.add.image(400, 150, "logo");
-this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
+
+  this.tweens.add({
+      targets: logo,
+      y: 450,
+      duration: 2000,
+      ease: "Power2",
+      yoyo: true,
+      loop: -1
+    });
 }
+
 ReactDOM.render(<App />, document.getElementById("root"));
